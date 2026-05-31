abd2ba5b55af4f7ec0744ef2713c35b028b7b125
Yibo Wang <2024312277@stu.hit.edu.cn>
2026-06-01 00:50:22 +0800

    Oplus: disable fastboot checks(Disable by default without -DENABLE_TESTING_PATCHS)

--- submodules/patcher/src/patchs/oplus/forceenablefastboot.c
+++ submodules/patcher/src/patchs/oplus/forceenablefastboot.c
@@ -14,45 +14,10 @@ bool patch_fastbootcheck(char* buffer, int32_t size, int32_t global_var_offset) 
     }
     return false;
 }
-bool patch_fastboot_olock1(char* buffer, int32_t size, int32_t global_var_offset) {
-    for (int32_t i = 0; i <= size - 4; i += 4) {
-        DecodedInst d = decode_at(buffer, i);
-        int64_t off0 = calc_adrl_file_offset(buffer, i+4, 0);
-        if (!str_at(buffer, size, off0, "if olock secure lock state, Not allow Fastboot.\n")) continue;
-        if (off0 < 0) continue;
-        if (d.type == INST_CBZ_W || d.type == INST_CBZ_X){//change to B
-            write_instr(buffer, i, change_to_b(d.raw));
-            printf("Patched fastboot warning jump at 0x%X to always jump, fastboot always enabled\n", i);
-            return true;
-        }
-    }
-    return false;
-}
-bool patch_fastboot_olock2(char* buffer, int32_t size, int32_t global_var_offset) {
-    for (int32_t i = 0; i <= size - 4; i += 4) {
-        DecodedInst d = decode_at(buffer, i);
-        int64_t off0 = calc_adrl_file_offset(buffer, i+4, 0);
-        if (!str_at(buffer, size, off0, "if olock secure lock state, Not allow Recovery:%d\n")) continue;
-        if (off0 < 0) continue;
-        if (d.type == INST_BCOND && d.cond == 1){//change to B
-            write_instr(buffer, i, change_to_b(d.raw));
-            printf("Patched fastboot warning jump at 0x%X to always jump, fastboot always enabled\n", i);
-            return true;
-        }
-    }
-    return false;
-}
+
 bool patch_fastboot(char* buffer, int32_t size, int32_t global_var_offset) {
     if (!patch_fastbootcheck(buffer, size, global_var_offset)) {
         printf("OPlus Warning: patch_fastbootcheck failed\n");
-        return false;
-    }
-    if (!patch_fastboot_olock1(buffer, size, global_var_offset)) {
-        printf("OPlus Warning: patch_fastboot_olock1 failed\n");
-        return false;
-    }
-    if (!patch_fastboot_olock2(buffer, size, global_var_offset)) {
-        printf("OPlus Warning: patch_fastboot_olock2 failed\n");
         return false;
     }
     return true;
